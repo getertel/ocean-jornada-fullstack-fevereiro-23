910077ef27cfae0965785ec50050e47ff9c0a227
Geter Campos de Cerqueira <106839529+getertel@users.noreply.github.com>
2023-02-24 10:13:40 -0300

    atualização feita na portaaa

--- backend/index.js
+++ backend/index.js
@@ -86,7 +86,9 @@ async function main() {
 
     res.send("Registro removido com sucesso!");
   });
-
+   const port = process.env.PORT || 3000;
+   app.listen(port, function(){console.log("Servidor rodandona porta: + port);
+  }); 
   app.listen(3000);
 }
 
